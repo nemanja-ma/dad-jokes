f5a91b9c6c2f7874c8e3d544b0bf940e1611dbd4
nemanja-ma <nemanja.front@gmail.com>
2019-10-30 12:53:29 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            joke: null,
+            isFetchingJoke: false
+        };
+
+        this.onTellJoke = this.onTellJoke.bind(this);
+    }
+
+componentDidMount() {
+    this.fetchJoke();
+}
+
+    fetchJoke() {
+        this.setState({isFetchingJoke: true});
+
+        fetch('https://icanhazdadjoke.com/', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(json => {
+                this.setState({ 
+                    joke: json.joke,
+                    isFetchingJoke: false 
+                });
+            });
+    };
+
+onTellJoke () {
+    this.fetchJoke();
+}
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" placeholder="Enter search term..."></input>
+                    <button> Search </button>
+                    <button 
+                    onClick={this.onTellJoke} 
+                    disabled={this.state.isFetchingJoke}
+                    > 
+                    Tell me a joke
+                    </button>
+                </form>
+                
+                <p>{this.state.isFetchingJoke ? 'Loading joke...' : this.state.joke}</p>
+            </div >
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
